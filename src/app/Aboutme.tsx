d1353fca15e9d6b13e42a1fa65da389faf0c850b
Abdulrahman Alali <22000577@ug.buid.ac.ae>
2024-01-16 18:25:03 +0400

    Added about me boiler plate

---
+++ src/app/Aboutme.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import HeaderText from "@/components/HeaderText";
+
+export default function Aboutme() {
+  return (
+    <>
+      <HeaderText text="About Me" />
+      <div className="text-center">
+        <p>
+          I love to learn new things and I am always looking for new challenges
+          everyday. I like contributing to open source software as well.
+        </p>
+      </div>
+    </>
+  );
+}
