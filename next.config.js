95ca76a0f00f0ea55004cd072dbac84bb59cbe15
Abdulrahman Alali <22000577@ug.buid.ac.ae>
2024-02-22 11:41:02 +0400

    Added button on click

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    unstable_runtimeJS: false,
     images: {
         
     }
--- src/app/Hero.tsx
+++ src/app/Hero.tsx
@@ -1,3 +1,4 @@
+"use client"
 import React from "react";
 import Image from "next/image";
 import { BiDownload } from "react-icons/bi";
@@ -10,13 +11,11 @@ export default function Hero() {
         <div className="xl:flex xl:items-center xl:ml-72 pt-32 xl:pt-0 xl:-translate-y-12 z-30 xl:h-screen">
           <div className="body-text text-center flex flex-col justify-center xl:pt-40 xl:text-left container mx-auto z-20">
             <h1 className="h1 text-5xl font-bold mb-4 leading-relaxed select-none">
-              Full-Stack <br />{" "}
-              <span className="text-blue-500">Web Developer</span>
+              Full-Stack <br /> <span className="text-blue-500">Web Developer</span>
             </h1>
             <p className="profile-description w-[22rem] xl:w-[44rem] xl:max-w-sl mx-auto xl:mx-0 mb-10 xl:mb-16 font-light select-none">
-              I am a dedicated full-stack developer, my focus is on creating
-              innovative solutions that streamline and enhance our daily lives.
-              I specialize in building applications that automate tasks.
+              I am a dedicated full-stack developer, my focus is on creating innovative solutions that streamline and enhance our daily lives. I specialize in building applications that automate
+              tasks.
             </p>
             <div className="container flex justify-center xl:justify-normal">
               <a href={"/CV.pdf"} download>
@@ -26,22 +25,20 @@ export default function Hero() {
                 </button>
               </a>
 
-              <button className="group relative overflow-hidden px-4 text-blue-500 rounded">
-                <span className="relative bg-blue-500 text-white rounded px-4 py-[0.63rem]">
-                  {"See all projects ->"}
-                </span>
+              <button
+                onClick={() => {
+                  const element = document.getElementById("projects");
+                  element?.scrollIntoView({ behavior: "smooth" });
+                }}
+                className="group relative overflow-hidden px-4 text-blue-500 rounded"
+              >
+                <span className="relative bg-blue-500 text-white rounded px-4 py-[0.63rem]">{"See all projects ->"}</span>
               </button>
             </div>
           </div>
           <div className="w-full xl:w-auto flex justify-center items-center">
             <div className="w-60 h-60 xl:w-80 xl:h-80 aspect-square rounded-full shadow-xl z-30 mt-24 xl:mr-48 mb-[4.5rem] xl:translate-y-12">
-              <Image
-                src={ProfilePicture}
-                alt="Profile Picture"
-                width={500}
-                height={500}
-                className="object-cover object-top w-full h-full rounded-full shadow-lg"
-              ></Image>
+              <Image src={ProfilePicture} alt="Profile Picture" width={500} height={500} className="object-cover object-top w-full h-full rounded-full shadow-lg"></Image>
             </div>
           </div>
         </div>
